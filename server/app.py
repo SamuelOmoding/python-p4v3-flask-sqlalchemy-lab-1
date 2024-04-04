af32856a0e018a9be5a1ef57075219744ddf5d72
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-04-04 08:57:03 +0300

    Assignment updates

--- server/app.py
+++ server/app.py
@@ -15,5 +15,13 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if not quake:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    return jsonify({"id": quake.id, "location": quake.location, "magnitude": quake.magnitude, "year": quake.year}), 200
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
